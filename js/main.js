2ae7947f7dd10015ad05912bec6317103042f5f6
Guilherme Barros <Guilhermescarmagnani@outlook.com>
2022-01-31 00:01:27 -0300

    refactor: Melhorias de codigo

--- js/main.js
+++ js/main.js
@@ -1,16 +1,20 @@
-function setColor(imgNumber){
+function setColorImgDetail(imgNumber){
     $('img').closest('div').css('border-left', '10px solid transparent')
     $(imgNumber).closest('div').css('border-left', '10px solid var(--mainColor)')
+}
+
+function setColorPage(color){
+    document.documentElement.style.setProperty('--mainColor', color);    
 }
 
 $('select').on('change', function (e) {
     var valueSelected = this.value;
     if(valueSelected == 'viperUltimateCyberpunk'){
-        document.documentElement.style.setProperty('--mainColor', '#FBEE46');
+        setColorPage('#FBEE46')
     }else if(valueSelected == 'viperUltimateRose'){
-        document.documentElement.style.setProperty('--mainColor', '#F2C5D6');
+        setColorPage('#F2C5D6')
     }else{
-        document.documentElement.style.setProperty('--mainColor', '#00DE00');
+        setColorPage('#00DE00')
     }
 });
 
@@ -18,18 +22,18 @@ $("div").children('img').click( function(e){
     var target = $( e.target );   
     if($(target).hasClass('img1')){
         $('.main_pic').css('background-image', 'url("./src/mouse1.png")')
-        setColor('img.img1')
+        setColorImgDetail('img.img1')
     }else if ((target).hasClass('img2')){
         $('.main_pic').css('background-image','url("./src/mouse2.png")')
-        setColor('img.img2')
+        setColorImgDetail('img.img2')
     }
     else if ((target).hasClass('img3')){
         $('.main_pic').css('background-image','url("./src/mouse3.png")')
-        setColor('img.img3')
+        setColorImgDetail('img.img3')
     }
     else if ((target).hasClass('img4')){
         $('.main_pic').css('background-image','url("./src/mouse4.png")')
-        setColor('img.img4')
+        setColorImgDetail('img.img4')
     }
 
 })
