3a4e4c07c7d0d96f571a822cdc551c8d1efeae53
Guilherme Barros <Guilhermescarmagnani@outlook.com>
2022-05-31 23:17:27 -0300

    fix: Correções de layout, melhorada visualização em notebooks, tablets, celulares grandes e medios, refatoramento de scss, refatoramento de js, melhorado tooltip, melhorado visualização de estilos em geral, diminuido tamanho da navabar etc..

--- js/main.js
+++ js/main.js
@@ -1,39 +1,76 @@
-function setColorImgDetail(imgNumber){
-    $('img').closest('div').css('border-left', '10px solid transparent')
-    $(imgNumber).closest('div').css('border-left', '10px solid var(--mainColor)')
+$(function(){
+    AtivarTooltip();
+    AtivarSelecaoDeModeloDeMouse();
+    AtivarTrocaDeImagemPrincipal();
+})
+
+function AtivarSelecaoDeModeloDeMouse(){
+    $('select').on('change', function (e) {
+        var valueSelected = this.value;
+        if(valueSelected == 'viperUltimateCyberpunk'){
+            setMainColor('#FBEE46')
+        }else if(valueSelected == 'viperUltimateRose'){
+            setMainColor('#F2C5D6')
+        }else{
+            setMainColor('#00DE00')
+        }
+    });
 }
 
-function setColorPage(color){
+function AtivarTooltip(){
+    $('.tooltip_dot').on('click' , function(e){
+        var target = $(e.target);
+        if( $(target).children().css('visibility') == 'hidden')
+            $(target).children().css('visibility','visible');
+        else
+            $(target).children().css('visibility','hidden');
+    })
+}
+
+function setImgNumber(target){
+    $(".mini_pics").find("img").css('box-shadow', '-12px 0px 0 transparent')
+    $(".mini_pics").find(`img.${target}`).css('box-shadow', '-12px 0px 0 var(--mainColor)')
+}
+
+function setMainColor(color){
     document.documentElement.style.setProperty('--mainColor', color);    
 }
 
-$('select').on('change', function (e) {
-    var valueSelected = this.value;
-    if(valueSelected == 'viperUltimateCyberpunk'){
-        setColorPage('#FBEE46')
-    }else if(valueSelected == 'viperUltimateRose'){
-        setColorPage('#F2C5D6')
+function TrocarDeImagemPrincipal(target){
+    $('.main_pic').css('background-image', 'url("./src/'+target+'.png")')
+    setImgNumber(target)
+    AjustarTooltip(target)
+}
+
+function AjustarTooltip(target){
+    if(target == "img1"){
+        $('.tooltips').css("display","block")
     }else{
-        setColorPage('#00DE00')
+        $('.tooltips').css("display","none")
     }
-});
+}
 
-$("div").children('img').click( function(e){
-    var target = $( e.target );   
-    if($(target).hasClass('img1')){
-        $('.main_pic').css('background-image', 'url("./src/mouse1.png")')
-        setColorImgDetail('img.img1')
-    }else if ((target).hasClass('img2')){
-        $('.main_pic').css('background-image','url("./src/mouse2.png")')
-        setColorImgDetail('img.img2')
-    }
-    else if ((target).hasClass('img3')){
-        $('.main_pic').css('background-image','url("./src/mouse3.png")')
-        setColorImgDetail('img.img3')
-    }
-    else if ((target).hasClass('img4')){
-        $('.main_pic').css('background-image','url("./src/mouse4.png")')
-        setColorImgDetail('img.img4')
-    }
+function AtivarTrocaDeImagemPrincipal(){
+    $(".mini_pics").find('img').click( function(e){
+        var e = $(e.target); 
+        var target = e[0].className
+    
+        switch(target){
+            case "img1":
+                TrocarDeImagemPrincipal(target)
+                break;
+            case "img2":
+                TrocarDeImagemPrincipal(target)
+                break;
+            case "img3":
+                TrocarDeImagemPrincipal(target)
+                break;
+            case "img4":
+                TrocarDeImagemPrincipal(target)
+                break;
+        }
+    
+    })
+}
 
-})
+
